a71f35126dd5ef65e04655cfd8ca5e5eefdf1457
naxiner <kirienkodenis2002@gmail.com>
2023-09-27 00:50:23 +0300

    Added the ability to delete a category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -61,5 +61,34 @@ namespace BulkyWeb.Controllers
 			}
 			return View();
 		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+
+			Category? category = _db.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+			return View(category);
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? id)
+		{
+			Category? category = _db.Categories.Find(id);
+			if (category == null)
+			{
+				return NotFound();
+			}
+
+			_db.Remove(category);
+			_db.SaveChanges();
+			return RedirectToAction("Index");
+		}
 	}
 }
