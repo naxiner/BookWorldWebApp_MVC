5a00d4c76ae40843ed39e955b5030e86d193df49
naxiner <kirienkodenis2002@gmail.com>
2023-10-01 23:15:33 +0300

    Moved to N-Tier architecture

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,5 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
-using Microsoft.AspNetCore.Http.HttpResults;
+﻿using Bulky.DataAcess.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
