4482d930d1ab3fe10aaf166fbffd6f962056470d
naxiner <kirienkodenis2002@gmail.com>
2023-11-15 23:07:33 +0200

    Added Identity Tables

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -19,7 +19,7 @@ namespace BookWorldWeb
 			
             builder.Services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-			
+            builder.Services.AddRazorPages();			
             builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
             var app = builder.Build();
@@ -38,7 +38,7 @@ namespace BookWorldWeb
             app.UseRouting();
 			app.UseAuthentication();
             app.UseAuthorization();
-
+            app.MapRazorPages();
             app.MapControllerRoute(
                 name: "default",
 				pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
