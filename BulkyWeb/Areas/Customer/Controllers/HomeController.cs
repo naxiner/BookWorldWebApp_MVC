e41f25c5015ff3934a8aba10daa03737317a03e1
naxiner <kirienkodenis2002@gmail.com>
2023-11-24 18:24:44 +0200

    Implimented Add to Cart

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 ﻿using BookWorld.DataAccess.Repository.IRepository;
 using BookWorld.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BookWorldWeb.Areas.Customer.Controllers
 {
@@ -26,12 +28,30 @@ namespace BookWorldWeb.Areas.Customer.Controllers
 
 		public IActionResult Details(int productId)
 		{
-			Product product =
-				_unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category");
-			return View(product);
+            ShoppingCart shoppingCart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+                ProductId = productId,
+                Count = 1
+            };
+			return View(shoppingCart);
 		}
 
-		public IActionResult Privacy()
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            _unitOfWork.ShoppingCart.Add(shoppingCart);
+            _unitOfWork.Save();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
