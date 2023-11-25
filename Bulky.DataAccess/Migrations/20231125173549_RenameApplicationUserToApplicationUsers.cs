16f04c044129b40fd2e190fd4eb0984720ff917d
naxiner <kirienkodenis2002@gmail.com>
2023-11-25 19:42:10 +0200

    Changed ApplicationUser to ApplicationUsers

---
+++ Bulky.DataAccess/Migrations/20231125173549_RenameApplicationUserToApplicationUsers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookWorld.DataAcess.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameApplicationUserToApplicationUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
