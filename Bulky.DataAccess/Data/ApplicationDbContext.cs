e8bf619d02aa1dd89aa8712173bc299c0d8960e1
naxiner <kirienkodenis2002@gmail.com>
2023-11-25 18:49:28 +0200

    Created OrderHeader an Detail Model

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -17,6 +17,8 @@ namespace BookWorld.DataAcess.Data
 		public DbSet<Company> Companies { get; set; }
 		public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 		public DbSet<ApplicationUser> ApplicationUser { get; set; }
+		public DbSet<OrderHeader> OrderHeaders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
 
 		protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
