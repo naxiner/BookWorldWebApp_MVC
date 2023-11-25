fb7f765c16722df0d804632e6bd04e1c003381d0
naxiner <kirienkodenis2002@gmail.com>
2023-11-25 22:48:09 +0200

    Added helper methods in OrderHeader Repository

--- Bulky.Models/OrderHeader.cs
+++ Bulky.Models/OrderHeader.cs
@@ -24,6 +24,7 @@ namespace BookWorld.Models
         public DateTime PaymentDate { get; set; }
         public DateTime PaymentDueDate { get; set; }
 
+        public string? SessionId { get; set; }
         public string? PaymentIntentId { get; set; }
 
         [Required]
