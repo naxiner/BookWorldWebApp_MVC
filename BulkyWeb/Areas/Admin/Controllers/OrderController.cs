08d9cdbd454991f89a782a15a6fa09ffb3807ca8
naxiner <kirienkodenis2002@gmail.com>
2023-11-26 17:28:03 +0200

    Added Filter logic and Order Details GET Action Method

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,7 +1,10 @@
 ﻿using BookWorld.DataAccess.Repository.IRepository;
 using BookWorld.Models;
+using BookWorld.Models.ViewModels;
+using BookWorld.Utility;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace BookWorldWeb.Areas.Admin.Controllers
 {
@@ -20,16 +23,45 @@ namespace BookWorldWeb.Areas.Admin.Controllers
 			return View();
 		}
 
+		public IActionResult Details(int orderId)
+		{
+			OrderVM orderVM = new()
+			{
+				OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, includeProperties: "ApplicationUser"),
+				OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, includeProperties: "Product")
+			};
+
+			return View(orderVM);
+		}
+
 		#region API CALLS
 
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
-			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			
+			switch (status)
+			{
+				case "pending":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+					break;
+				case "inprocess":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+					break;
+				case "completed":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusShipped);
+					break;
+				case "approved":
+					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.StatusApproved);
+					break;
+				default:
+					break;
+			}
+
 			return Json(new { data = objOrderHeaders });
 		}
 
 		#endregion
-
 	}
 }
