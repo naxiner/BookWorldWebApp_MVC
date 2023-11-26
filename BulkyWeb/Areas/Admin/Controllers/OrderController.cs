33983fe563226958c526baffebf010f53ffde7cc
naxiner <kirienkodenis2002@gmail.com>
2023-11-26 18:53:57 +0200

    Only Admin and Employee can see all Orders

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -3,9 +3,8 @@ using BookWorld.Models;
 using BookWorld.Models.ViewModels;
 using BookWorld.Utility;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using System.Security.Claims;
 
 namespace BookWorldWeb.Areas.Admin.Controllers
 {
@@ -73,7 +72,20 @@ namespace BookWorldWeb.Areas.Admin.Controllers
 		{
 			IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
 			
-			switch (status)
+			if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+			{
+                objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            }
+			else
+			{
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+				objOrderHeaders = _unitOfWork.OrderHeader
+					.GetAll(u => u.ApplicationUserId == userId, includeProperties: "ApplicationUser");
+            }
+
+            switch (status)
 			{
 				case "pending":
 					objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
