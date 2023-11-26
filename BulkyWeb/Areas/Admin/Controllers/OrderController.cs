267296a7565bbc55bd332b587ba2f6d5a30ca863
naxiner <kirienkodenis2002@gmail.com>
2023-11-26 16:27:10 +0200

    Added Order Controller and ViewModel

---
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using BookWorld.DataAccess.Repository.IRepository;
+using BookWorld.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookWorldWeb.Areas.Admin.Controllers
+{
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _unitOfWork;
+
+		public OrderController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		#region API CALLS
+
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+			return Json(new { data = objOrderHeaders });
+		}
+
+		#endregion
+
+	}
+}
