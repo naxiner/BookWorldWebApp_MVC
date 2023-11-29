b7336cc30b82683f05950f41e59bb0f576b51d36
naxiner <kirienkodenis2002@gmail.com>
2023-11-29 21:49:27 +0200

    Only Admin can create Admin and Employee accounts

--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -184,6 +184,7 @@ namespace BookWorldWeb.Areas.Customer.Controllers
                     _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                     _unitOfWork.Save();
 				}
+                HttpContext.Session.Clear();
             }
 
             List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart
