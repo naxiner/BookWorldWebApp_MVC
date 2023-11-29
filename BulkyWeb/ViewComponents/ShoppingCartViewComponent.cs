b595ebff6fc028b41d8532e87415d33cc3ca5fa9
naxiner <kirienkodenis2002@gmail.com>
2023-11-29 21:19:21 +0200

    Added View Component

---
+++ BulkyWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,41 @@
+﻿using BookWorld.DataAccess.Repository.IRepository;
+using BookWorld.Utility;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookWorldWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            
+            if (claim != null && claim.Value != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                        _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
+
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+
+                return View(HttpContext.Session.GetInt32(SD.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
